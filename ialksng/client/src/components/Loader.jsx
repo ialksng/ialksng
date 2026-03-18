d489878e361cdb4170b24d5336ed24dcc3f38ba7
Alok Singh <ialksng@gmail.com>
2026-03-19 02:21:24 +0530

    loader

--- ialksng/client/src/components/Loader.jsx
+++ ialksng/client/src/components/Loader.jsx
@@ -1,12 +1,9 @@
 import "../styles/loader.css";
-import logo from "../assets/logo.png";
 
 function Loader() {
   return (
     <div className="loader-wrapper">
-      <div className="logo-loader">
-        <img src={logo} alt="loading" />
-      </div>
+      <div className="spinner"></div>
     </div>
   );
 }
