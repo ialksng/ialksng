8bda7c2a519e44fa54edf31cd1e4f1a234b75053
Alok Singh <ialksng@gmail.com>
2026-03-18 18:30:29 +0530

    Update ViewProduct.jsx

--- ialksng/client/src/pages/ViewProduct.jsx
+++ ialksng/client/src/pages/ViewProduct.jsx
@@ -1,6 +1,6 @@
 import { useParams, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-import "../styles/viewProduct.css";
+import "../styles/viewproduct.css";
 
 function ViewProduct() {
   const { id } = useParams();
