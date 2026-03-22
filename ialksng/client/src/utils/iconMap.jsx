df111acc47c0b6c1c12e55d40c26129504819ce3
Alok Singh <ialksng@gmail.com>
2026-03-22 15:12:14 +0530

    Update iconMap.jsx

--- ialksng/client/src/utils/iconMap.jsx
+++ ialksng/client/src/utils/iconMap.jsx
@@ -1,20 +1,52 @@
-import { FaReact, FaNodeJs, FaPython, FaJava, FaGitAlt, FaAws } from "react-icons/fa";
-import { SiMongodb, SiMysql, SiExpress, SiJavascript, SiDocker, SiHtml5, SiCss, SiBootstrap, SiTailwindcss } from "react-icons/si";
+import { 
+  FaReact, FaNodeJs, FaPython, FaJava, FaGitAlt, FaAws, FaGithub, 
+  FaDocker, FaChartLine, FaChartBar, FaHtml5, FaCss3Alt, FaBootstrap 
+} from "react-icons/fa";
+import { 
+  SiMongodb, SiMysql, SiExpress, SiJavascript, SiTailwindcss, 
+  SiSpringboot, SiPostgresql, SiGithubactions, SiKubernetes, 
+  SiMicrosoftexcel, SiPowerbi, SiNumpy, SiPandas 
+} from "react-icons/si";
+import { TbSql, TbApi } from "react-icons/tb";
 
 export const iconMap = {
-  FaReact: <FaReact />,
-  FaNodeJs: <FaNodeJs />,
-  FaPython: <FaPython />,
+  // Languages
   FaJava: <FaJava />,
-  FaGitAlt: <FaGitAlt />,
-  FaAws: <FaAws />,
-  SiMongodb: <SiMongodb />,
-  SiMysql: <SiMysql />,
-  SiExpress: <SiExpress />,
   SiJavascript: <SiJavascript />,
-  SiDocker: <SiDocker />,
-  SiHtml5: <SiHtml5 />,
-  SiCss: <SiCss />,
-  SiBootstrap: <SiBootstrap />,
+  FaPython: <FaPython />,
+  TbSql: <TbSql />,
+
+  // Frontend
+  FaReact: <FaReact />,
+  FaHtml5: <FaHtml5 />,       
+  FaCss3Alt: <FaCss3Alt />,   
+  FaBootstrap: <FaBootstrap />, 
   SiTailwindcss: <SiTailwindcss />,
+
+  // Backend
+  SiSpringboot: <SiSpringboot />,
+  FaNodeJs: <FaNodeJs />,
+  SiExpress: <SiExpress />,
+  TbApi: <TbApi />,
+
+  // Databases
+  SiMysql: <SiMysql />,
+  SiPostgresql: <SiPostgresql />,
+  SiMongodb: <SiMongodb />,
+
+  // DevOps & Tools
+  FaGitAlt: <FaGitAlt />,
+  FaGithub: <FaGithub />,
+  SiGithubactions: <SiGithubactions />,
+  FaDocker: <FaDocker />,      
+  SiKubernetes: <SiKubernetes />,
+  FaAws: <FaAws />,
+
+  // Others
+  SiMicrosoftexcel: <SiMicrosoftexcel />,
+  SiPowerbi: <SiPowerbi />,
+  SiNumpy: <SiNumpy />,
+  SiPandas: <SiPandas />,
+  FaChartLine: <FaChartLine />, // Using generic line chart for Matplotlib
+  FaChartBar: <FaChartBar />,   // Using generic bar chart for Seaborn
 };
