d9b2869130e91c7c5adb7a46199bc32d2421ea05
Alok Singh <ialksng@gmail.com>
2026-03-27 23:46:16 +0530

    Update certificationRoutes.js

--- ialksng/server/routes/certificationRoutes.js
+++ ialksng/server/routes/certificationRoutes.js
@@ -1,15 +1,15 @@
 import express from "express";
 import { getCertifications, addCertification, deleteCertification } from "../controllers/certificationController.js";
 import { protect } from "../middleware/authMiddleware.js";
-import { admin } from "../middleware/adminMiddleware.js";
+import { adminOnly } from "../middleware/adminMiddleware.js";
 
 const router = express.Router();
 
 router.route("/")
   .get(getCertifications)
-  .post(protect, admin, addCertification);
+  .post(protect, adminOnly, addCertification);
 
 router.route("/:id")
-  .delete(protect, admin, deleteCertification);
+  .delete(protect, adminOnly, deleteCertification);
 
 export default router;
