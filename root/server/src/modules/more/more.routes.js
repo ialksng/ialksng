8185da640ce7fa175c0546e0d425cc20505c8f0c
Alok Singh <ialksng@gmail.com>
2026-04-18 15:11:10 +0530

    Update more.routes.js

--- root/server/src/modules/more/more.routes.js
+++ root/server/src/modules/more/more.routes.js
@@ -11,9 +11,9 @@ router.get('/products', getProducts);
 router.get('/life', getLifePosts);
 
 router.post('/games', adminOnly, createGame);
-router.delete('/games/:id', verifyAdmin, deleteGame);
-router.delete('/streams/:id', verifyAdmin, deleteStream);
-router.delete('/products/:id', verifyAdmin, deleteProduct);
-router.delete('/life/:id', verifyAdmin, deleteLifePost);
+router.delete('/games/:id', adminOnly, deleteGame);
+router.delete('/streams/:id', adminOnly, deleteStream);
+router.delete('/products/:id', adminOnly, deleteProduct);
+router.delete('/life/:id', adminOnly, deleteLifePost);
 
 export default router;
