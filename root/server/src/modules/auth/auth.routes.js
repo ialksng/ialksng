b05e6b7358c7a316211680f3ca8c2bb0112ce472
Alok Singh <ialksng@gmail.com>
2026-04-18 07:19:11 +0530

    Update auth.routes.js

--- root/server/src/modules/auth/auth.routes.js
+++ root/server/src/modules/auth/auth.routes.js
@@ -1,22 +1,16 @@
 import express from "express";
 
-import {
-  registerUser,
-  loginUser,
-  getMe,
-  updateProfile,
-  changePassword,
-  submitFeedback
-} from "./auth.controller.js";
-
+import { signup, login, googleAuth, getMe, sendForgotPasswordOTP, resetPasswordWithOTP } from "./auth.controller.js";
 import { protect } from "../../core/middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/register", registerUser);
-router.post("/login", loginUser);
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/google", googleAuth);
 router.get("/me", protect, getMe);
-
+router.post("/forgot-password/send-otp", sendForgotPasswordOTP);
+router.post("/forgot-password/reset", resetPasswordWithOTP);
 router.put("/profile", protect, updateProfile);
 router.put("/change-password", protect, changePassword);
 router.post("/feedback", protect, submitFeedback);
